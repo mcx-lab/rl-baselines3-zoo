d74626ee92242501b0d9ac54c4580ff7dc5fd6b8
jennyzzt <jennylive158@gmail.com>
2021-09-02 17:19:33 +0800

    Updated BaseVelocitySensor with the option to be adjusted to robot's local frame, change action limit to 0.3

--- blind_walking/envs/env_builder.py
+++ blind_walking/envs/env_builder.py
@@ -46,7 +46,7 @@ def build_regular_env(robot_class,
       simulation_parameters=sim_params)
 
   sensors = [
-    robot_sensors.BaseVelocitySensor(),
+    robot_sensors.BaseVelocitySensor(convert_to_local_frame=True, exclude_z=True),
     robot_sensors.IMUSensor(channels=['R', 'P', 'Y', 'dR', 'dP', 'dY']),
     robot_sensors.MotorAngleSensor(num_motors=a1.NUM_MOTORS),
     robot_sensors.MotorVelocitySensor(num_motors=a1.NUM_MOTORS)
