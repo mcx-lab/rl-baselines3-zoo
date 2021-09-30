04cca5a8fc1fcf4f7f753a754b57a9c444922419
Daniel Tan <dtch009@gmail.com>
2021-09-30 14:33:09 +0800

    Add simplified target position based task

--- blind_walking/envs/env_builder.py
+++ blind_walking/envs/env_builder.py
@@ -55,11 +55,12 @@ def build_regular_env(robot_class,
 
   env_sensor_list = [
     environment_sensors.LastActionSensor(num_actions=a1.NUM_MOTORS),
+    environment_sensors.TargetPositionSensor()
   ]
 
   env_randomizer_list = []
 
-  task = forward_task.ForwardTask()
+  task = forward_task_pos.ForwardTask()
 
   env = locomotion_gym_env.LocomotionGymEnv(gym_config=gym_config,
                                             robot_class=robot_class,
@@ -68,7 +69,6 @@ def build_regular_env(robot_class,
                                             task=task,
                                             env_randomizers=env_randomizer_list)
 
-  # env = obs_split_wrapper.ObservationDictionarySplitByEncoderWrapper(env)
   env = obs_array_wrapper.ObservationDictionaryToArrayWrapper(env)
   if (motor_control_mode
       == robot_config.MotorControlMode.POSITION) and wrap_trajectory_generator:
