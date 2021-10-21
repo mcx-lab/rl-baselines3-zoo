346c1d0fd3118a018547137e94d4b45dafc1df36
Daniel Tan <dtch1997@stanford.edu>
2021-10-21 16:43:36 +0800

    Update experrimental params

--- blind_walking/envs/env_builder.py
+++ blind_walking/envs/env_builder.py
@@ -61,9 +61,6 @@ def build_regular_env(
         env_sensor_list = [
             environment_sensors.LastActionSensor(num_actions=a1.NUM_MOTORS),
             # environment_sensors.PhaseSensor(init_angle=0, frequency=1.0),  # set frequency = 1 / (gait_cycle_len)
-            environment_sensors.LocalTerrainViewSensor(
-                enc_name="visual", grid_size=(20, 10), grid_unit=0.05, transform=(0.15, 0)
-            ),
         ]
 
     if env_randomizer_list is None:
