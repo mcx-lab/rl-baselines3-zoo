cc2efcea3f42fc3a10f3d4b7be6f406a94eb8eb6
jennyzzt <jennylive158@gmail.com>
2021-10-21 13:41:41 +0800

    Linter fixing

--- blind_walking/envs/locomotion_gym_env.py
+++ blind_walking/envs/locomotion_gym_env.py
@@ -353,7 +353,7 @@ class LocomotionGymEnv(gym.Env):
         base_pos = self._robot.GetBasePosition()
         view_matrix = self._pybullet_client.computeViewMatrixFromYawPitchRoll(
             cameraTargetPosition=(base_pos[0], 0, 0),
-            distance=3*self._camera_dist,
+            distance=3 * self._camera_dist,
             yaw=self._camera_yaw,
             pitch=self._camera_pitch,
             roll=0,
