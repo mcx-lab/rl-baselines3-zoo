19153a598410e6bc95cd9d4ac7f19374c2cbee2b
Daniel Tan <dtch1997@alumni.stanford.edu>
2021-11-08 22:38:12 +0800

    Refactor plotting functionality / arguments

--- blind_walking/examples/plot_depth_data.py
+++ blind_walking/examples/plot_depth_data.py
@@ -19,27 +19,27 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--raw-depth-path", type=str, help="Path to the raw depth sensor data")
     parser.add_argument("--filtered-depth-path", type=str, help="Path to the filtered depth data")
-    parser.add_argument("--savedir", type=str, help="Directory to save plots", default="plots")
+    parser.add_argument("--save-path", type=str, help="Directory to save plots", default="plots/depth_and_filtered.png")
     args = parser.parse_args()
 
     raw_depth = np.load(args.raw_depth_path)  # (episode_len, num_rays)
     filtered_depth = np.load(args.filtered_depth_path)
 
-    savedir = Path(args.savedir)
-    savedir.mkdir(exist_ok=True, parents=True)
+    save_path = Path(args.save_path)
+    save_path.parent.mkdir(exist_ok=True, parents=True)
 
     plt.figure()
     plt.plot(np.arange(raw_depth.shape[1]), raw_depth[0])
-    plt.savefig(str(savedir / "sanity_check.png"))
+    plt.savefig(str(save_path.parent / "sanity_check.png"))
 
     rays = [0, 10, 19]
     num_rays = len(rays)
-    fig, ax = plt.subplots(1, num_rays, figsize=(15, 5))
+    fig, ax = plt.subplots(num_rays, 1, figsize=(10, 10))
     for i, ray in enumerate(rays):
-        plot_signal(ax[i], raw_depth[:, ray], label=f"Raw depth, ray {ray}", alpha=0.5)
-        plot_signal(ax[i], filtered_depth[:, ray], label=f"Filtered depth, ray {ray}", alpha=0.5)
+        plot_signal(ax[i], raw_depth[:, ray], label=f"Raw depth, ray {ray}")
+        plot_signal(ax[i], filtered_depth[:, ray], label=f"Filtered depth, ray {ray}")
         ax[i].legend()
-    plt.savefig(str(savedir / "depth_and_filtered.png"))
+    plt.savefig(str(save_path))
 
 
 if __name__ == "__main__":
