Fix the calculation of hover_robot to not use noisy rpy values, decreased the heightmap noise

blind_walking/envs/sensors/environment_sensors.py
@@ -598,7 +598,8 @@ class LocalTerrainDepthSensor(sensor.BoxSpaceSensor):
             ).reshape(1, self.grid_size[0], self.grid_size[1])
 
         if self._noisy_reading:
-            heightmap = heightmap + np.random.normal(scale=0.05, size=heightmap.shape)
+            heightmap = heightmap + np.random.normal(scale=0.01, size=heightmap.shape)
+            heightmap = np.maximum(heightmap, 0)
         return heightmap
 
 

blind_walking/examples/hover_robot.py
@@ -152,7 +152,7 @@ def main():
         data = np.load(datapath)
 
         # Plot one data point of the heightmap
-        for i in range(20):
+        for i in range(grid_size[0]):
             plotter = Plotter(datapath, f"hm_single{i}")
             plotter.plot(columns=[i], ylim=(0.2, 0.8), savedir=dirpath)
 