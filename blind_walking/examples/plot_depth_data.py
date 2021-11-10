f58368b8e82e52f3bbeb13c917c6505a0ef2db34
Daniel Tan <dtch1997@alumni.stanford.edu>
2021-11-10 22:51:27 +0800

    Refactor plotting scripts

--- blind_walking/examples/plot_depth_data.py
+++ blind_walking/examples/plot_depth_data.py
@@ -1,6 +1,7 @@
 """Script for plotting depth sensor data, raw and filtered."""
 
 import argparse
+import itertools
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -15,31 +16,66 @@ def plot_signal(ax: plt.Axes, signal: np.ndarray, label: str, **kwargs):
     ax.set_ylabel("Signal value")
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--raw-depth-path", type=str, help="Path to the raw depth sensor data")
-    parser.add_argument("--filtered-depth-path", type=str, help="Path to the filtered depth data")
-    parser.add_argument("--save-path", type=str, help="Directory to save plots", default="plots/depth_and_filtered.png")
-    args = parser.parse_args()
-
-    raw_depth = np.load(args.raw_depth_path)  # (episode_len, num_rays)
-    filtered_depth = np.load(args.filtered_depth_path)
-
-    save_path = Path(args.save_path)
-    save_path.parent.mkdir(exist_ok=True, parents=True)
-
+def plot_depth_and_filtered(raw_depth: np.ndarray, filtered_depth: np.ndarray, plot_path: Path):
     plt.figure()
     plt.plot(np.arange(raw_depth.shape[1]), raw_depth[0])
-    plt.savefig(str(save_path.parent / "sanity_check.png"))
+    plt.savefig(str(plot_path.parent / "sanity_check.png"))
 
-    rays = [0, 10, 19]
+    rays = [0, 5, 9]
     num_rays = len(rays)
     fig, ax = plt.subplots(num_rays, 1, figsize=(10, 10))
     for i, ray in enumerate(rays):
         plot_signal(ax[i], raw_depth[:, ray], label=f"Raw depth, ray {ray}")
         plot_signal(ax[i], filtered_depth[:, ray], label=f"Filtered depth, ray {ray}")
         ax[i].legend()
-    plt.savefig(str(save_path))
+    plt.savefig(str(plot_path))
+
+
+def load_data(stats_path: Path, data_prefix: str, env_modifier: str, is_observation_modified: bool):
+    return np.load(stats_path / f"{get_data_name(data_prefix, env_modifier, is_observation_modified)}.npy")
+
+
+def get_data_name(data_prefix: str, env_modifier: str, is_observation_modified: bool):
+    modify_suffix = "modified" if is_observation_modified else "unmodified"
+    return f"{data_prefix}_{env_modifier}_{modify_suffix}"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--stats-path", help="Directory in which statistics are saved", type=str)
+    parser.add_argument(
+        "--terrain-modifiers",
+        type=str,
+        nargs="+",
+        help="Terrain modifiers to examine",
+        default=["heightfield", "stairs_0", "stairs_1"],
+    )
+    parser.add_argument("--modification-flags", type=int, nargs="+", default=[0, 1])
+    parser.add_argument("--plot-path", type=str, help="Directory to save plots", default=None)
+    args = parser.parse_args()
+
+    # Create plot directory
+    stats_path = Path(args.stats_path)
+    plot_path = args.plot_path
+    if plot_path is None:
+        plot_path = stats_path.parent / "plots"
+    else:
+        plot_path = Path(args.plot_path)
+    plot_path.mkdir(exist_ok=True, parents=True)
+
+    # Plot all data
+    env_modifiers = args.terrain_modifiers
+    is_observation_modifieds = args.modification_flags
+
+    for settings in itertools.product(env_modifiers, is_observation_modifieds):
+        env_modifier, is_observation_modified = settings
+        raw_depth = load_data(stats_path, "raw_depth", env_modifier, is_observation_modified)
+        filtered_depth = load_data(stats_path, "filtered_depth", env_modifier, is_observation_modified)
+        plot_depth_and_filtered(
+            raw_depth,
+            filtered_depth,
+            plot_path / get_data_name("raw_and_filtered_depth", env_modifier, is_observation_modified),
+        )
 
 
 if __name__ == "__main__":
