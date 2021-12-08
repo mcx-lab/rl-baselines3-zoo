598faa029cdc1dbd8e5f5bf904ae9d0e068ecfbd
Daniel Tan <dtch1997@alumni.stanford.edu>
2021-12-08 12:49:39 +0800

    Remove level up / down curriculum, replace stairs with steps

--- blind_walking/envs/env_modifiers/train_course.py
+++ blind_walking/envs/env_modifiers/train_course.py
@@ -14,7 +14,7 @@ class TrainStairs(EnvModifier):
         super().__init__()
         self.step_rise_levels = [0.02, 0.05, 0.075, 0.10]
         self.num_levels = len(self.step_rise_levels)
-        self.num_steps = 10
+        self.num_steps = 1
         self.stair_gap = 1.5
         self.step_run = 0.3
         self.stair_length = (self.num_steps - 1) * self.step_run * 2 + boxHalfLength * 2 * 2
@@ -34,48 +34,7 @@ class TrainStairs(EnvModifier):
             start_x += self.stair_length + self.stair_gap
 
     def _reset(self, env):
-        if self._level > 0 and self.down_level(env):
-            # robot down-levels
-            self._level -= 1
-            print(f"DOWNGRADE TO LEVEL {self._level}")
-        elif self._level < self.num_levels and self.up_level(env):
-            # robot up-levels
-            self._level += 1
-            print(f"LEVEL UP TO LEVEL {self._level}!")
-        level = self._level
-        if level >= self.num_levels:
-            # Loop back to randomly selected level
-            level_list = np.arange(self.num_levels) + 1
-            level_probs = level_list / sum(level_list)
-            level = np.random.choice(self.num_levels, p=level_probs)
-            print(f"LOOP TO LEVEL {level}")
-
-        x_pos = level * (self.stair_length + self.stair_gap)
-        z_pos = 0
-        # Equal chances to encouter going up and down the stair level
-        if np.random.uniform() < 0.4:
-            x_pos += self.stair_gap + self.stair_length / 2 - 1
-            z_pos = self.step_rise_levels[level] * self.num_steps
-        self.adjust_position = (x_pos, 0, z_pos)
-
-    def up_level(self, env):
-        """To succeed the current level, robot needs to climb over the current stair level
-        and reach the start of next stair level"""
-        base_pos = env._robot.GetBasePosition()
-        target_x = (self._level + 1) * (self.stair_length + self.stair_gap) + 0.5
-        return (
-            self.adjust_position[2] == 0
-            and base_pos[0] > target_x
-            and base_pos[1] > -boxHalfWidth
-            and base_pos[1] < boxHalfWidth
-        )
-
-    def down_level(self, env):
-        """Downgrade to the previous level if robot was unable to travel a quarter of the stair length"""
-        start_pos = self.adjust_position
-        base_pos = env._robot.GetBasePosition()
-        x_dist_travelled = base_pos[0] - start_pos[0]
-        return x_dist_travelled < self.stair_length / 5
+        pass
 
 
 class TrainUneven(EnvModifier):
