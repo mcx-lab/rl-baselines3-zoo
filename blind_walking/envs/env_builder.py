0cf85b5f4725408cc07fe14bb863422a7f269398
Daniel Tan <dtch1997@alumni.stanford.edu>
2021-12-08 14:07:20 +0800

    Reduce command vel to 0.5 m/s

--- blind_walking/envs/env_builder.py
+++ blind_walking/envs/env_builder.py
@@ -61,7 +61,7 @@ def build_regular_env(
     if env_sensor_list is None:
         env_sensor_list = [
             environment_sensors.LastActionSensor(num_actions=a1.NUM_MOTORS),
-            environment_sensors.ForwardTargetPositionSensor(max_distance=0.04),
+            environment_sensors.ForwardTargetPositionSensor(max_distance=0.015),
             environment_sensors.LocalTerrainDepthSensor(
                 grid_size=(7, 7),
                 grid_unit=(0.05, 0.05),
