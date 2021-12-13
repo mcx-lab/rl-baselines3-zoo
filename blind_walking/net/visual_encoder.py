889298aaa5e04ebc079225633c2b0fa9557352a2
Daniel Tan <dtch1997@alumni.stanford.edu>
2021-12-13 14:07:20 +0800

    Add pretrained encoder, reset training hyperparams

--- blind_walking/net/visual_encoder.py
+++ blind_walking/net/visual_encoder.py
@@ -1,4 +1,5 @@
 import math
+from pathlib import Path
 
 import gym
 import torch as th
@@ -6,6 +7,13 @@ from blind_walking.net.utilities import conv_output_shape
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.type_aliases import TensorDict
 from torch import nn
+
+from utils import ALGOS
+
+
+def load_encoder(model_path: Path) -> th.nn.Module:
+    model = ALGOS["ppo"].load(model_path, env=None)
+    return model.policy.features_extractor
 
 
 class LocomotionVisualEncoder(BaseFeaturesExtractor):
@@ -17,7 +25,7 @@ class LocomotionVisualEncoder(BaseFeaturesExtractor):
     To be used with ObservationDictionarySplitByEncoderWrapper
     """
 
-    def __init__(self, observation_space: gym.spaces.Dict, visual_output_size: int = 16):
+    def __init__(self, observation_space: gym.spaces.Dict, visual_output_size: int = 16, load_pretrained=False):
         self.visual_output_size = visual_output_size
         flatten_output_size = math.prod(observation_space.spaces["flatten"].shape)
         visual_input_size = math.prod(observation_space.spaces["visual"].shape)
@@ -27,6 +35,22 @@ class LocomotionVisualEncoder(BaseFeaturesExtractor):
         self.flatten_encoder = nn.Flatten()
         # 1-layer MLP extractor
         self.visual_encoder = nn.Sequential(nn.Linear(visual_input_size, visual_output_size), nn.ReLU())
+
+        # Load the saved encoder from a previous run
+        if load_pretrained:
+            print("Loading pretrained encoder")
+            from copy import deepcopy
+
+            initial_weights = deepcopy(self.visual_encoder.state_dict())
+            pretrained_encoder = load_encoder(Path("mcx_logs") / "ppo" / "A1GymEnv-v0_4" / "A1GymEnv-v0.zip")
+            self.visual_encoder.load_state_dict(pretrained_encoder.visual_encoder.state_dict())
+
+            # Some basic sanity checks to ensure weights were loaded
+            for key, value in self.visual_encoder.state_dict().items():
+                print(f"Checking parameters: {key}")
+                assert key in initial_weights
+                assert th.all(initial_weights[key] != value)
+            print("Loaded weights successfully")
 
     def forward(self, observations: TensorDict) -> th.Tensor:
         assert set(observations.keys()) == set(["flatten", "visual"])
