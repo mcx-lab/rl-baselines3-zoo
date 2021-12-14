406effd19623b8adf7968999d43e82eb7c3b682e
jennyzzt <jennylive158@gmail.com>
2021-12-14 15:21:04 +0800

    Commented out env creation in rospackage, added custom observation msg

--- quadruped_ros/scripts/dummy_obs.py
+++ quadruped_ros/scripts/dummy_obs.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
+import sys
+import os
 import rospy
 from std_msgs.msg import String
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from quadruped_ros.msg import Observation
 
 
 if __name__ == "__main__":
     try:
         rospy.init_node("quadruped_simulation", anonymous=True)
-        pub = rospy.Publisher("observations", String, queue_size=100)
+        pub = rospy.Publisher("observations", Observation, queue_size=1)
         rate = rospy.Rate(33)  # hz
         while not rospy.is_shutdown():
             rospy.loginfo(f"hello world! {rospy.get_time()}")
-            msg = String()
-            msg.data = "hello world!"
+            msg = Observation()
+            msg.data = [1.0] * 92
             pub.publish(msg)
             rate.sleep()
     except rospy.ROSInterruptException:
--- quadruped_ros/scripts/rl_algo.py
+++ quadruped_ros/scripts/rl_algo.py
@@ -11,12 +11,19 @@ from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3 import PPO
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-import utils.import_envs  # noqa: F401 pylint: disable=unused-import
-from utils import create_test_env, get_saved_hyperparams
+# import utils.import_envs  # noqa: F401 pylint: disable=unused-import
+# from utils import create_test_env, get_saved_hyperparams
+from quadruped_ros.msg import Observation
+
+
+cb_obs = Observation()
+cb_obs.data = [[0.0] * 92]
 
 
 def callback_rlalgo(obs):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", obs.data)
+    global cb_obs
+    cb_obs = obs
+    rospy.loginfo(rospy.get_caller_id() + "I heard smth!")
 
 
 def main():  # noqa: C901
@@ -28,32 +35,32 @@ def main():  # noqa: C901
     env_id = "A1GymEnv-v0"
     log_path = args.log_path
 
-    # ######################### Create environment ######################### #
+    # # ######################### Create environment ######################### #
 
-    set_random_seed(args.seed)
-    stats_path = os.path.join(log_path, env_id)
-    hyperparams, stats_path = get_saved_hyperparams(stats_path, norm_reward=False, test_mode=True)
+    # set_random_seed(args.seed)
+    # stats_path = os.path.join(log_path, env_id)
+    # hyperparams, stats_path = get_saved_hyperparams(stats_path, norm_reward=False, test_mode=True)
 
-    # load env_kwargs if existing
-    env_kwargs = {}
-    args_path = os.path.join(log_path, env_id, "args.yml")
-    if os.path.isfile(args_path):
-        with open(args_path, "r") as f:
-            loaded_args = yaml.load(f, Loader=yaml.UnsafeLoader)  # pytype: disable=module-attr
-            if loaded_args["env_kwargs"] is not None:
-                env_kwargs = loaded_args["env_kwargs"]
+    # # load env_kwargs if existing
+    # env_kwargs = {}
+    # args_path = os.path.join(log_path, env_id, "args.yml")
+    # if os.path.isfile(args_path):
+    #     with open(args_path, "r") as f:
+    #         loaded_args = yaml.load(f, Loader=yaml.UnsafeLoader)  # pytype: disable=module-attr
+    #         if loaded_args["env_kwargs"] is not None:
+    #             env_kwargs = loaded_args["env_kwargs"]
 
-    env = create_test_env(
-        env_id,
-        n_envs=1,
-        stats_path=stats_path,
-        seed=args.seed,
-        log_dir=None,
-        should_render=False,
-        hyperparams=hyperparams,
-        env_kwargs=env_kwargs,
-    )
-    obs = env.reset()
+    # env = create_test_env(
+    #     env_id,
+    #     n_envs=1,
+    #     stats_path=stats_path,
+    #     seed=args.seed,
+    #     log_dir=None,
+    #     should_render=False,
+    #     hyperparams=hyperparams,
+    #     env_kwargs=env_kwargs,
+    # )
+    # obs = env.reset()
 
     # ######################### Load model ######################### #
 
@@ -75,13 +82,13 @@ def main():  # noqa: C901
     # ######################### ROS node ######################### #
 
     rospy.init_node("rl_algo", anonymous=True)
-    rospy.Subscriber("observations", String, callback_rlalgo)
-    rospy.spin()
-    # rate = rospy.Rate(33)  # hz
-    # while not rospy.is_shutdown():
-    #     action, _ = model.predict(obs, state=None, deterministic=True)
-    #     obs, reward, done, infos = env.step(action)
-    #     rate.sleep()
+    rospy.Subscriber("observations", Observation, callback_rlalgo)
+    rate = rospy.Rate(33)  # hz
+    while not rospy.is_shutdown():
+        obs = cb_obs.data
+        action, _ = model.predict(obs, state=None, deterministic=True)
+        # obs, reward, done, infos = env.step(action)
+        rate.sleep()
 
 
 if __name__ == "__main__":
