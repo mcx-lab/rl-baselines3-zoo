33fcc894ea0fa7425f57668ba19cea69c3ab4054
dtch1997 <dtch1997@alumni.stanford.edu>
2022-02-11 14:26:07 +0800

    Refactor ROS communication code

--- quadruped_ros/scripts/rl_algo.py
+++ quadruped_ros/scripts/rl_algo.py
@@ -8,84 +8,39 @@ import sys
 import pickle
 from stable_baselines3 import PPO
 from sensor_msgs.msg import Imu, JointState
-from nav_msgs.msg import Odometry
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from quadruped_ros.msg import (
-    QuadrupedLegPos,
-    QuadrupedLeg,
-    IMUSensor,
+    MotorCmds,
     BaseVelocitySensor,
-    TargetPositionSensor,
-    HeightmapSensor,
 )
 from blind_walking.robots.action_filter import ActionFilterButter
-
-_obs_basevelocity = [0.0] * 2
-_obs_imu = [0.0] * 6
-_obs_motors = [0.0] * 24
-_obs_lastaction = [0.0] * 12
-_obs_targetpos = [0.0] * 2
-_obs_heightmap = [0.0] * 10
-
-
-def callback_basevelocity(obs):
-    global _obs_basevelocity
-    _obs_basevelocity[0] = obs.vx
-    _obs_basevelocity[1] = obs.vy
-
-
-def callback_imu(obs):
-    global _obs_imu
-    _obs_imu[0] = obs.linear_acceleration.x
-    _obs_imu[1] = obs.linear_acceleration.y
-    _obs_imu[2] = obs.linear_acceleration.z
-
-    _obs_imu[3] = obs.angular_velocity.x
-    _obs_imu[4] = obs.angular_velocity.y
-    _obs_imu[5] = obs.angular_velocity.z
-
-
-def callback_motors(obs):
-    global _obs_motors
-    # ['FR_hip_joint', 'FR_upper_joint', 'FR_lower_joint',
-    # 'FL_hip_joint', 'FL_upper_joint', 'FL_lower_joint', 
-    # 'RR_hip_joint', 'RR_upper_joint', 'RR_lower_joint', 
-    # 'RL_hip_joint', 'RL_upper_joint', 'RL_lower_joint'] 
-    _obs_motors[:12] = obs.position
-    _obs_motors[12:] = obs.velocity
-
-
-def callback_targetpos(obs):
-    global _obs_targetpos
-    _obs_targetpos[0] = obs.dx
-    _obs_targetpos[1] = obs.dy
-
-
-def callback_heightmap(obs):
-    global _obs_heightmap
-    _obs_heightmap = list(obs.data)
 
 LAIKAGO_DEFAULT_ABDUCTION_ANGLE = 0
 LAIKAGO_DEFAULT_HIP_ANGLE = 0.67
 LAIKAGO_DEFAULT_KNEE_ANGLE = -1.25
-LAIKAGO_DEFAULT_POSE = np.array([
-    LAIKAGO_DEFAULT_ABDUCTION_ANGLE,
-    LAIKAGO_DEFAULT_HIP_ANGLE,
-    LAIKAGO_DEFAULT_KNEE_ANGLE,
-] * 4)
+LAIKAGO_DEFAULT_POSE = np.array(
+    [
+        LAIKAGO_DEFAULT_ABDUCTION_ANGLE,
+        LAIKAGO_DEFAULT_HIP_ANGLE,
+        LAIKAGO_DEFAULT_KNEE_ANGLE,
+    ]
+    * 4
+)
+
 
 def nn_action_to_motor_angle(action: np.ndarray) -> np.ndarray:
-    """ Converts NN action to a motor angle in radians. 
+    """Converts NN action to a motor angle in radians.
 
     Adapted from blind_walking.env_wrappers.simple_openloop.LaikagoPoseOffsetGenerator
     """
     assert action.shape == LAIKAGO_DEFAULT_POSE.shape
     return LAIKAGO_DEFAULT_POSE + action
 
+
 def build_action_filter():
-    """ Constructs action filter used in simulation. 
-    
+    """Constructs action filter used in simulation.
+
     Adapted from minitaur.Minitaur._BuildActionFilter
     """
     sampling_rate = 1 / (0.001 * 30)
@@ -96,72 +51,116 @@ def build_action_filter():
 
     return a_filter
 
+
 def filter_action(action_filter: ActionFilterButter, robot_action: np.ndarray):
-    """ Applies action filter used in simulation. 
-    
+    """Applies action filter used in simulation.
+
     Adapted from minitaur.Minitaur._FilterAction
     """
     filtered_action = action_filter.filter(robot_action)
     return filtered_action
 
-def main():  # noqa: C901
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--log-path", help="Path to folder containing pre-trained model", type=str, default="./")
-    args, _ = parser.parse_known_args()
 
-    env_id = "A1GymEnv-v0"
-    log_path = args.log_path
+class ROSController:
+    """Interface for sending and receiving messages over ROS"""
+
+    def __init__(self):
+        # Subscriber variables
+        self._obs_base_velocity = [0.0] * 2
+        self._obs_imu_state = [0.0] * 6
+        self._obs_motor_positions = [0.0] * 12
+        self._obs_motor_velocities = [0.0] * 12
+
+        # Publisher variables
+        self._motor_commands_msg = MotorCmds()
+
+        # Initialize ROS node
+        rospy.init_node("NeuralNetController", anonymous=True)
+
+        # Set up subscribers
+        rospy.Subscriber("obs_base_velocity", BaseVelocitySensor, self.on_receive_base_velocity)
+        rospy.Subscriber("obs_imu_state", Imu, self.on_receive_imu_state)
+        rospy.Subscriber("obs_motor_state", JointState, self.on_receive_motor_state)
+
+        # Set up publishers
+        self.motor_command_publisher = rospy.Publisher("motor_commands", MotorCmds, queue_size=3)
+
+    def on_receive_base_velocity(self, obs):
+        """Callback for updating base velocity from ROS topic"""
+        self._obs_basevelocity[0] = obs.vx
+        self._obs_basevelocity[1] = obs.vy
+
+    def on_receive_imu_state(self, obs):
+        """Callback for updating IMU state from ROS topic"""
+        self._obs_imu_state[0] = obs.linear_acceleration.x
+        self._obs_imu_state[1] = obs.linear_acceleration.y
+        self._obs_imu_state[2] = obs.linear_acceleration.z
+
+        self._obs_imu_state[3] = obs.angular_velocity.x
+        self._obs_imu_state[4] = obs.angular_velocity.y
+        self._obs_imu_state[5] = obs.angular_velocity.z
+
+    def on_receive_motor_state(self, obs):
+        """Callback for updating motor state from ROS topic"""
+        self._obs_motor_positions = obs.position
+        self._obs_motor_velocities = obs.velocity
+
+    def publish_action(self, motor_commands: np.ndarray):
+        """Callback for publishing motor commands over ROS topics"""
+        self._motor_commands_msg.motorCmds = motor_commands
+        self.motor_command_publisher.publish(self._motor_commands_msg)
+
+    def get_robot_observation(self):
+        return np.concatenate(
+            [
+                self._obs_base_velocity,
+                self._obs_imu_state,
+                self._obs_motor_positions,
+                self._obs_motor_velocities,
+            ]
+        )
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--log-path", type=str, help="Path to folder containing pre-trained model")
+    parser.add_argument("--env-id", type=str, default="A1GymEnv-v0", help="ID of OpenAI gym env to lookup trained model for")
+    return parser.parse_args()
+
+
+def main():  # noqa: C901
+
+    args = get_args()
 
     # ######################### Load model ######################### #
 
-    model_path = os.path.join(log_path, f"{env_id}.zip")
+    model_path = os.path.join(args.log_path, f"{args.env_id}.zip")
     model = PPO.load(model_path, deterministic=True)
     print(f"Loaded model from {model_path}")
 
-    vecnorm_path = os.path.join(log_path, env_id, "vecnormalize.pkl")
+    # Set up normalization wrapper
+    vecnorm_path = os.path.join(args.log_path, args.env_id, "vecnormalize.pkl")
     with open(vecnorm_path, "rb") as f:
         vecnorm = pickle.load(f)
 
-    # ######################### ROS node ######################### #
-
-    global _obs_lastaction
-
+    # Set up action filter
     action_filter = build_action_filter()
 
-    rospy.init_node("rl_algo", anonymous=True)
-    rospy.Subscriber("obs_basevelocity", BaseVelocitySensor, callback_basevelocity)
-    rospy.Subscriber("obs_imu", Imu, callback_imu)
-    rospy.Subscriber("obs_motors", JointState, callback_motors)  # motor angles and velocity
-    rospy.Subscriber("obs_targetpos", TargetPositionSensor, callback_targetpos)
-    rospy.Subscriber("obs_heightmap", HeightmapSensor, callback_heightmap)
-    pub_action = rospy.Publisher("actions", QuadrupedLegPos, queue_size=10)
+    # ######################### ROS node ######################### #
+
+    controller = ROSController()
+
+    # Hardcode target_pos to be in forward direction
+    target_pos = np.array([0.02, 0.0])
+
     rate = rospy.Rate(33)  # hz
     while not rospy.is_shutdown():
-        obs = _obs_basevelocity + _obs_imu + _obs_motors + _obs_lastaction + _obs_targetpos + _obs_heightmap
-        # normalise observation
-        norm_obs = vecnorm.normalize_obs(obs)
-        # predict action
-        nn_action, _ = model.predict(norm_obs, state=None, deterministic=True)
-        nn_action = np.clip(nn_action, -0.5, 0.5)
-        robot_action = nn_action_to_motor_angle(nn_action) 
-        robot_action = filter_action(action_filter, robot_action)
-        # publish action
-        msg_action = QuadrupedLegPos()
-        msg_action.fr.hip.pos = robot_action[0]
-        msg_action.fr.upper.pos = robot_action[1]
-        msg_action.fr.lower.pos = robot_action[2]
-        msg_action.fl.hip.pos = robot_action[3]
-        msg_action.fl.upper.pos = robot_action[4]
-        msg_action.fl.lower.pos = robot_action[5]
-        msg_action.br.hip.pos = robot_action[6]
-        msg_action.br.upper.pos = robot_action[7]
-        msg_action.br.lower.pos = robot_action[8]
-        msg_action.bl.hip.pos = robot_action[9]
-        msg_action.bl.upper.pos = robot_action[10]
-        msg_action.bl.lower.pos = robot_action[11]
-        pub_action.publish(msg_action)
-        # update stored observation
-        _obs_lastaction = list(robot_action)
+        raw_obs = np.concatenate([controller.get_robot_observation(), target_pos])
+        norm_obs = vecnorm.normalize_obs(raw_obs)
+        nn_action = model.predict(norm_obs, deterministic=True)
+        motor_angles = nn_action_to_motor_angle(nn_action)
+        motor_angles = filter_action(action_filter, motor_angles)
+
         rate.sleep()
 
 
