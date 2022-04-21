d60808ee1dbf6151817ffc54b0b747182c38d576
dtch1997 <dtch1997@alumni.stanford.edu>
2022-04-21 18:01:07 +0800

    Add ability to sense future states as well as current state

--- blind_walking/envs/sensors/cpg_sensors.py
+++ blind_walking/envs/sensors/cpg_sensors.py
@@ -42,6 +42,7 @@ class ReferenceGaitSensor(sensor.BoxSpaceSensor):
         gait_frequency_upper: float = DEFAULT_GAIT_FREQUENCY,
         duty_factor_lower: float = DEFAULT_DUTY_FACTOR,
         duty_factor_upper: float = DEFAULT_DUTY_FACTOR,
+        obs_steps_ahead: typing.List[int] = [0],
         lower_bound: _FLOAT_OR_ARRAY = -np.pi,
         upper_bound: _FLOAT_OR_ARRAY = np.pi,
         name: typing.Text = "ReferenceGait",
@@ -73,6 +74,7 @@ class ReferenceGaitSensor(sensor.BoxSpaceSensor):
         self._get_foot_contact = foot_contact_fn[gait_name]
         self._gait_frequency_range = (gait_frequency_lower, gait_frequency_upper)
         self._duty_factor_range = (duty_factor_lower, duty_factor_upper)
+        self._obs_steps_ahead = obs_steps_ahead
 
         self.cpg_system = CPGSystem(
             params=params,
@@ -83,7 +85,7 @@ class ReferenceGaitSensor(sensor.BoxSpaceSensor):
 
         super(ReferenceGaitSensor, self).__init__(
             name=name,
-            shape=(4,),
+            shape=(4 * len(self._obs_steps_ahead),),
             enc_name=enc_name,
             lower_bound=lower_bound,
             upper_bound=upper_bound,
@@ -97,15 +99,27 @@ class ReferenceGaitSensor(sensor.BoxSpaceSensor):
 
         self.cpg_system.step()
         self._current_phase = self.cpg_system.get_phase()
+        obs = self._get_foot_contact(self._current_phase)
+        self._obs_history_buffer.append(obs)
+        self._obs_history_buffer.pop(0)
 
     def _reset(self):
-        # Reset to a random state
+        # Clear the history buffer
+        self._obs_history_buffer = []
+        # Reset CPG to a random state
         gait_frequency = np.random.uniform(self._gait_frequency_range[0], self._gait_frequency_range[1])
         self.set_period(1 / gait_frequency)
         duty_factor = np.random.uniform(self._duty_factor_range[0], self._duty_factor_range[1])
         self.set_duty_factor(duty_factor)
         self.cpg_system.set_state(CPGSystem.sample_initial_state(self._phase_offset))
         self._current_phase = self.cpg_system.get_phase()
+
+        # Initialize the history buffer
+        for i in range(10):
+            self.cpg_system.step()
+            self._current_phase = self.cpg_system.get_phase()
+            obs = self._get_foot_contact(self._current_phase)
+            self._obs_history_buffer.append(obs)
 
     def on_reset(self, env):
         """From the callback, the sensor remembers the environment.
@@ -119,7 +133,14 @@ class ReferenceGaitSensor(sensor.BoxSpaceSensor):
         """Returns np.ndarray of shape (4,)
 
         obs[i] = 1 iff foot[i] should be in contact with ground and -1 otherwise"""
-        return self._get_foot_contact(self._current_phase)
+        obses = [self._obs_history_buffer[obs_idx] for obs_idx in self._obs_steps_ahead]
+        return np.concatenate(obses)
+
+    def get_current_reference_state(self):
+        """
+        Gets the correct reference foot contact state at the current time.
+        """
+        return self._obs_history_buffer[0]
 
     def get_period(self):
         return self.cpg_system.params.period
